16b29a12d9bfe6c1f0d46aabbd90181bc1ad0c4f
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-01-25 18:36:08 +0000

    Add generic validator errors type

--- src/models/validation/Validator.model.ts
+++ src/models/validation/Validator.model.ts
@@ -1,6 +1,10 @@
 import { Validated } from "./Validated.model";
+import { ValidationErrors } from "./ValidationErrors.model";
 
 /**
  * @typeParam T - the validated type
+ * @typeParam U - the validation errors type
  */
-export type Validator<T> = (obj: unknown) => Validated<T, string>;
+export type Validator<T, U extends ValidationErrors<T> = string> = (
+  obj: unknown
+) => Validated<T, U>;
