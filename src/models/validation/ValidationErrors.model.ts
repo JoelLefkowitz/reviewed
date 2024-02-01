5f346b5312b326da0d2537a115f974c867d8b812
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-02-01 14:06:23 +0000

    Drop arrays from ValidationErrors type

--- src/models/validation/ValidationErrors.model.ts
+++ src/models/validation/ValidationErrors.model.ts
@@ -1,5 +1,9 @@
+type NotArray<T> = T extends Array<unknown> ? never : unknown;
+
 /**
  * @typeParam T - the validated type
  */
-export type ValidationErrors<T> =
-  T extends Record<string, unknown> ? Partial<Record<keyof T, string>> : never;
+export type ValidationErrors<T> = T extends NotArray<T> &
+  Record<string, unknown>
+  ? Partial<Record<keyof T, string>>
+  : never;
