b907654d5fee19f60dce86b99875e71413f6e455
Joel Lefkowitz <joel.lefkowitz@teya.co>
2024-02-01 14:16:03 +0000

    Allow all RecordLike types in ValidationErrors type

--- src/models/validation/ValidationErrors.model.ts
+++ src/models/validation/ValidationErrors.model.ts
@@ -1,9 +1,11 @@
-type NotArray<T> = T extends Array<unknown> ? never : unknown;
+type NotArrayLike<T> = T extends ArrayLike<unknown> ? never : unknown;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type RecordLike<T> = T extends Record<string, any> ? unknown : never;
 
 /**
  * @typeParam T - the validated type
  */
-export type ValidationErrors<T> = T extends NotArray<T> &
-  Record<string, unknown>
+export type ValidationErrors<T> = T extends NotArrayLike<T> & RecordLike<T>
   ? Partial<Record<keyof T, string>>
   : never;
