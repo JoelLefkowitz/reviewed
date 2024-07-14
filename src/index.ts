79b0df2ff4b9ce8550d642a5ae4d86ed06df5741
Joel Lefkowitz <joel.lefkowitz@teya.com>
2024-07-14 16:42:02 +0100

    Add isRecordOfAtLeast and optional to exports

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,54 @@
-export { isArrayOf, isRecordOf } from "./factories/aliases";
-export { validateEach, validateAll } from "./factories/arrays";
-export { validateIf } from "./factories/conditionals";
-export { fail } from "./factories/errors";
-export { validateOr, validateEachOr } from "./factories/fallbacks";
-export { filterValid } from "./factories/filter";
-export { guard, fromGuard } from "./factories/guards";
-export { invalidate, invalidateWith } from "./factories/invalidate";
-export { validateRegex } from "./factories/regexes";
-export { all, any, merge, sieve } from "./factories/results";
-export { not, both, either } from "./factories/transform";
-export { validate, validateWith } from "./factories/validate";
+export { Annotated, RecordLike } from "./models/records";
 export { ArrayFields, ValidatedFields, ValidatorFields } from "./models/fields";
-export { RecordLike, Annotated } from "./models/records";
-export { RegexResult, RegexValidator } from "./models/regexes";
 export {
-  Valid,
-  Invalid,
-  ValidationErrors,
-  Validated,
-  Validator,
   Guard,
+  Invalid,
+  Valid,
+  Validated,
+  ValidationErrors,
+  Validator,
 } from "./models/validators";
+export { RegexResult, RegexValidator } from "./models/regexes";
+export { all, any, merge, sieve } from "./factories/results";
+export { both, either, not, optional } from "./factories/transform";
+export { fail } from "./factories/errors";
+export { filterValid } from "./factories/filter";
+export { fromGuard, guard } from "./factories/guards";
+export { invalidate, invalidateWith } from "./factories/invalidate";
+export {
+  isArray,
+  isManyOf,
+  isNonEmptyArray,
+  isNumberArray,
+  isOneOf,
+  isStringArray,
+} from "./validators/arrays";
+export { isArrayOf, isRecordOf, isRecordOfAtLeast } from "./factories/aliases";
+export {
+  isBoolean,
+  isNull,
+  isNumber,
+  isObject,
+  isString,
+  isUndefined,
+} from "./validators/primitives";
+export {
+  isBooleanString,
+  isIntegerString,
+  isNaturalNumberString,
+  isNumberString,
+} from "./validators/strings";
+export { isEmail } from "./validators/regexes";
+export { isInteger, isNaturalNumber } from "./validators/numbers";
+export { isNonEmptyRecord, isRecord } from "./validators/records";
 export { serialize } from "./services/strings";
 export { suite } from "./testing/suites";
 export {
-  isArray,
-  isNonEmptyArray,
-  isNumberArray,
-  isStringArray,
-  isOneOf,
-  isManyOf,
-} from "./validators/arrays";
-export { isInteger, isNaturalNumber } from "./validators/numbers";
-export {
-  isUndefined,
-  isNull,
-  isBoolean,
-  isNumber,
-  isString,
-  isObject,
-} from "./validators/primitives";
-export { isRecord, isNonEmptyRecord } from "./validators/records";
-export { isEmail } from "./validators/regexes";
-export {
-  isBooleanString,
-  isNumberString,
-  isIntegerString,
-  isNaturalNumberString,
-} from "./validators/strings";
+  validate,
+  validateWith,
+  validateWithAtLeast,
+} from "./factories/validate";
+export { validateAll, validateEach } from "./factories/arrays";
+export { validateEachOr, validateOr } from "./factories/fallbacks";
+export { validateIf } from "./factories/conditionals";
+export { validateRegex } from "./factories/regexes";
